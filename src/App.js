22d3c8748fdf299400bc371d87c46e2ba3c23467
omarelloumi <48672001+omarelloumi@users.noreply.github.com>
2022-09-16 10:41:48 +0100

    Create project structure and routing

--- src/App.js
+++ src/App.js
@@ -1,8 +1,17 @@
+import { Routes, Route } from "react-router-dom";
+import Container from '@mui/material/Container';
+import Home from "./pages/Home";
+import About from "./pages/About";
+import Store from "./pages/Store";
 function App() {
   return (
-    <>
-      App
-    </>
+    <Container>
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/about" element={<About />} />
+        <Route path="/store" element={<Store />} />
+      </Routes>  
+    </Container>
   );
 }
 
