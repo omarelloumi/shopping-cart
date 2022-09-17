1ccf3bcf95935861126f1f9d73c9e04fd5a1f015
omarelloumi <48672001+omarelloumi@users.noreply.github.com>
2022-09-17 12:22:37 +0100

    format shopping cart

--- src/components/ShoppingCart/ShoppingCart.jsx
+++ src/components/ShoppingCart/ShoppingCart.jsx
@@ -1,16 +1,15 @@
-import React from 'react'
-import ShoppingCartIcon from '@mui/icons-material/ShoppingCart';
-import useStyles from './styles';
+import React from "react";
+import ShoppingCartIcon from "@mui/icons-material/ShoppingCart";
+import useStyles from "./styles";
 
 const ShoppingCart = () => {
-    const classes = useStyles()
+  const classes = useStyles();
   return (
-        <button className={classes.cartButton} >
-            <div className={classes.cartCount}>0</div>
-            <ShoppingCartIcon className={classes.cartImage} />
+    <button className={classes.cartButton}>
+      <div className={classes.cartCount}>0</div>
+      <ShoppingCartIcon className={classes.cartImage} />
+    </button>
+  );
+};
 
-        </button>
-  )
-}
-
-export default ShoppingCart
+export default ShoppingCart;
