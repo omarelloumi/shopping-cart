8618b92ae308d1c410b9621e76880b1b5c4c7f60
omarelloumi <48672001+omarelloumi@users.noreply.github.com>
2022-09-17 12:21:55 +0100

    format cart styles

--- src/components/ShoppingCart/styles.js
+++ src/components/ShoppingCart/styles.js
@@ -1,35 +1,34 @@
-import {createUseStyles} from 'react-jss'
+import { createUseStyles } from "react-jss";
 
-const useStyles = createUseStyles(
-    {
-        cartButton: {
-            position: 'relative',
-            backgroundColor: 'transparent',
-            border: 'none',
-            cursor: 'pointer',
-            width: '3rem',
-            height: '3rem',
-            '&:hover': {
-                color: 'lightblue',
-                backgroundColor: '#F0F8FF',
-                borderRadius: '40%',
-            },
-        },
-        cartImage: {
-            color: '#000',
-            '&:hover': {
-            color: '#318CE7',
-            },
-        },
-        cartCount: {
-            position: 'absolute',
-            top: '-5px',
-            right: '-5px',
-            backgroundColor: 'red',
-            color: '#fff',
-            borderRadius: '50%',
-            padding: '0 5px',
-            fontSize: '0.8rem',
-        },
-})
-export default useStyles
+const useStyles = createUseStyles({
+  cartButton: {
+    position: "relative",
+    backgroundColor: "transparent",
+    border: "none",
+    cursor: "pointer",
+    width: "3rem",
+    height: "3rem",
+    "&:hover": {
+      color: "lightblue",
+      backgroundColor: "#F0F8FF",
+      borderRadius: "40%",
+    },
+  },
+  cartImage: {
+    color: "#000",
+    "&:hover": {
+      color: "#318CE7",
+    },
+  },
+  cartCount: {
+    position: "absolute",
+    top: "-5px",
+    right: "-5px",
+    backgroundColor: "red",
+    color: "#fff",
+    borderRadius: "50%",
+    padding: "0 5px",
+    fontSize: "0.8rem",
+  },
+});
+export default useStyles;
